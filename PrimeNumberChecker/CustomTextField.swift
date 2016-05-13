8351fe5ae51fbdb471d9e9012dedb73264604c33
Adam Thuvesen <a.thuvesen@gmail.com>
2016-05-13 16:36:51 +0200

    Inital commit, before stack view testing

---
+++ PrimeNumberChecker/CustomTextField.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTextField.swift
+//  PrimeNumberChecker
+//
+//  Created by Adam Thuvesen on 2016-05-13.
+//  Copyright © 2016 Adam Thuvesen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCustomTextField: UITextField {
+    required init(coder aDecoder: (NSCoder!)) {
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 25
+        self.layer.borderColor = UIColor.clearColor().CGColor
+        self.layer.borderWidth = 1.5
+       // self.layer.opacity = 0.5
+        self.backgroundColor = UIColor.lightGrayColor()
+        self.textColor = UIColor.whiteColor()
+        self.textAlignment = .Center
+        self.tintColor = UIColor.whiteColor()
+        self.keyboardAppearance = .Dark
+        self.keyboardType = .NumberPad
+        self.placeholder = "Enter a number"
+        
+    }
+}
--- PrimeNumberChecker/ViewController.swift
+++ PrimeNumberChecker/ViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var enteredValue = 0
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var checkImage: UIImageView!
+    @IBOutlet weak var enterTextField: MyCustomTextField!
+    
+    @IBOutlet weak var wrongImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        
+        self.enterTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func calculateAction(sender: AnyObject) {
+        
+        dismissKeyboard()
+        
+        if enterTextField.text != nil && enterTextField.text != "" {
+            
+            enteredValue = Int(enterTextField.text!)!
+            
+            if isPrime(enteredValue) {
+                checkImage.hidden = false
+                print("Prime!")
+            } else {
+                wrongImage.hidden = false
+                print("Not prime!")
+            }
+        }
     }
-
+    
+    func isPrime(n: Int) -> Bool {
+        if n <= 1 {
+            return false
+        }
+        
+        if n <= 3 {
+            return true
+        }
+        
+        var i = 2
+        while i*i <= n {
+            if n % i == 0 {
+                return false
+            }
+            i += 1
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        checkImage.hidden = true
+        wrongImage.hidden = true
+        print("Editing...")
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            logoImage.hidden = true
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            logoImage.hidden = false
+            self.view.frame.origin.y += keyboardSize.height
+        }
+    }
 
 }
 
